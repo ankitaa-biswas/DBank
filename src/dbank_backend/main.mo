d06595c6c72fbc7ba50e8983f07d6009e306c575
Ankita Biswas <ankita@Ankitas-MacBook-Air.local>
2025-02-28 19:03:27 +0530

    Updated code with fixes

--- src/dbank_backend/main.mo
+++ src/dbank_backend/main.mo
@@ -2,12 +2,18 @@ import Debug "mo:base/Debug";
 import Float "mo:base/Float";
 
 import Time "mo:base/Time";
+
+
+
 actor DBank{
  stable var currentValue: Float = 300.00;
-  // currentValue := 300;
+  currentValue := 300;
+Debug.print(debug_show(currentValue));
+
+
 
 stable var startTime = Time.now();
-// startTime:=Time.now();
+startTime:=Time.now();
 Debug.print(debug_show(startTime));
 
 
@@ -28,7 +34,7 @@ let id=34355;
      currentValue-=amount;
     Debug.print(debug_show(currentValue));
   }else{
-    Debug.print("amount too large")
+    Debug.print("amount too large");
   }
 
  
@@ -45,7 +51,7 @@ public func compound(){
   let  currentTime = Time.now();
   let timeElapsedNS = currentTime - startTime;
   let timeElapsedS = timeElapsedNS / 1000000000;
-  currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS));
+  currentValue := currentValue * (1.001 ** Float.fromInt(timeElapsedS));
   startTime := currentTime;
 }
 
--- src/dbank_frontend/index.js
+++ src/dbank_frontend/index.js
@@ -1,3 +1,61 @@
+import { FuncClass } from "@dfinity/candid/lib/cjs/idl";
+import {dbank_backend} from "../declarations/dbank_backend";
+
+
+window.addEventListener("load", async function() {
+  // console.log("Finished loading");
+    update();
+
+ 
+   });
+  
+
+   document.querySelector("form").addEventListener("submit",async function(event){
+    event.preventDefault();
+    // console.log("Submitted");
+
+
+
+    const button  = event.target.querySelector("#submit-btn");
+
+
+    const inputAmount = parseFloat(document.getElementById("input-amount").value);
+    const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+    button.setAttribute("disabled",true);
+
+    if (document.getElementById("input-amount").value.length != 0){
+      await dbank_backend.topUp(inputAmount); 
+    }
+
+   
+
+    if (document.getElementById("withdrawal-amount").value.length != 0){
+      await dbank_backend.withdraw(outputAmount); 
+    }
+
+
+
+    await dbank_backend.compound();
+  update();
+
+    document.getElementById("input-amount").value="";
+    document.getElementById("withdrawal-amount").value="";
+    button.removeAttribute("disabled",);
+   })
+
+
+async function update(){
+
+  const currentAmount =await dbank_backend.checkBal();
+  document.getElementById("value").innerText=parseFloat(currentAmount).toFixed(2);
+}
+
+
+
+
+
+
+
 // import { dbank_backend } from "../declarations/dbank_backend";
 
 // window.addEventListener("load", async function() {
@@ -41,43 +99,108 @@
 //   document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
 // };
 
-import { dbank_backend } from "../declarations/dbank_backend";
+// import { dbank_backend } from "../declarations/dbank_backend";
 
-window.addEventListener("load", async function() {
-  // console.log("Finished loading");
-  update();
-});
+// window.addEventListener("load", async function() {
+//   // console.log("Finished loading");
+//   update();
+// });
 
-document.querySelector("form").addEventListener("submit", async function(event) {
-  event.preventDefault();
-  // console.log("Submitted.");
+// document.querySelector("form").addEventListener("submit", async function(event) {
+//   event.preventDefault();
+//   // console.log("Submitted.");
 
-  const button = event.target.querySelector("#submit-btn");
+//   const button = event.target.querySelector("#submit-btn");
 
-  const inputAmount = parseFloat(document.getElementById("input-amount").value);
-  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+//   const inputAmount = parseFloat(document.getElementById("input-amount").value);
+//   const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
 
-  button.setAttribute("disabled", true);
+//   button.setAttribute("disabled", true);
 
-  if (document.getElementById("input-amount").value.length != 0) {
-    await dbank_backend.topUp(inputAmount);
-  }
+//   if (document.getElementById("input-amount").value.length != 0) {
+//     await dbank_backend.topUp(inputAmount);
+//   }
 
-  if (document.getElementById("withdrawal-amount").value.length != 0) {
-    await dbank_backend.withdraw(outputAmount);
-  }
+//   if (document.getElementById("withdrawal-amount").value.length != 0) {
+//     await dbank_backend.withdraw(outputAmount);
+//   }
 
-  await dbank_backend.compound();
+//   await dbank_backend.compound();
 
-  update();
+//   update();
 
-  document.getElementById("input-amount").value = "";
-  document.getElementById("withdrawal-amount").value = "";
+//   document.getElementById("input-amount").value = "";
+//   document.getElementById("withdrawal-amount").value = "";
 
-  button.removeAttribute("disabled");
-});
+//   button.removeAttribute("disabled");
+// });
 
-async function update() {
-  const currentAmount = await dbank_backend.checkBal();
-  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
-};
+// async function update() {
+//   const currentAmount = await dbank_backend.checkBal();
+//   document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { dbank_backend } from "../declarations/dbank_backend";
+
+// window.addEventListener("load", async function() {
+//   // console.log("Finished loading");
+//   update();
+// });
+
+// document.querySelector("form").addEventListener("submit", async function(event) {
+//   event.preventDefault();
+//   // console.log("Submitted.");
+
+//   const button = event.target.querySelector("#submit-btn");
+
+//   const inputAmount = parseFloat(document.getElementById("input-amount").value);
+//   const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+//   button.setAttribute("disabled", true);
+
+//   if (document.getElementById("input-amount").value.length != 0) {
+//     await dbank_backend.topUp(inputAmount);
+//   }
+
+//   if (document.getElementById("withdrawal-amount").value.length != 0) {
+//     await dbank_backend.withdraw(outputAmount);
+//   }
+
+//   await dbank_backend.compound();
+
+//   update();
+
+//   document.getElementById("input-amount").value = "";
+//   document.getElementById("withdrawal-amount").value = "";
+
+//   button.removeAttribute("disabled");
+// });
+
+// async function update() {
+//   const currentAmount = await dbank_backend.checkBal();
+//   document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+// };
+
+
+
+
